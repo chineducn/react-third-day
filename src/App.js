c02773be33dd451863ac43c32ee12a5a3859f5f3
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-09-11 11:16:48 +0200

    wip

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Cart from './Cart';
 import Fruits from './Fruits';
 import './App.css';
@@ -16,6 +16,21 @@ function Market() {
   const addToCart = (item) => {
     setCart(cart.concat(item));
   };
+
+  // by invoking useEffect passing a callback "foo"
+  // we are telling the browser:
+  // hey, react! Once you are done updating the DOM,
+  // go ahead and invoke the callback "foo"
+  useEffect(() => {
+    // THIS EFFECT RUNS THE FIRST TIME Market renders,
+    // and every time Market re-renders
+    // (rendering being the WHOLE process)
+    console.log('Market first renders, OR re-rendered for whatever reason');
+  });
+
+  useEffect(() => {
+    console.log('Market first rendered. You will not see mee again');
+  }, []);
 
   return (
     <div className="App">
