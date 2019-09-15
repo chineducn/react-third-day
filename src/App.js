be5071a54607d99ba73ab4670a1dcaa47f420975
unknown <neduvinci@gmail.com>
2019-09-15 19:15:38 +0100

    set the data from the fruits api to the fruitstock state

--- src/App.js
+++ src/App.js
@@ -9,32 +9,36 @@ const meatsApi = 'http://localhost:4000/market/munchies';
 
 function Market() {
   
-  // const [stockState, setStockState] = useState({
-  //   fruitStock: ['udara', 'ichekwu', 'sour chop', 'mango', 'avocado'],
-  //   munchiesStock: ['date', 'suya', 'puff-puff', 'springroll', 'kilishi']
-  // })
   const [stockState, setStockState] = useState({
     fruitStock: [],
     munchiesStock: []
   })
   
   const { fruitStock, munchiesStock } = stockState;
+  // console.log(fruitStock)
   
   const [cartState, setCartState] = useState([]);
+
+  const [errorState, setErrorState] = useState(null);
 
   useEffect(() => {
     axios.get(fruitsApi)
       .then(fruitImport => {
-        // debugger
+        
         const { data } = fruitImport;
-        console.log(data);
-    })
-  });
+        // debugger
+        setStockState({ fruitStock: data, munchiesStock:[]});
+      })
+      .catch(errorItem => {
+        setErrorState(errorItem.message);
+      }
+    )
+  }, []);
    
 
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>{errorState}</h1>
       {/* <Fruit name={nameApp}/> */}
       <Items itemsArray={fruitStock} cartFunction={setCartState} cartArray={cartState}/>
       <Items itemsArray={munchiesStock} cartFunction={setCartState} cartArray={cartState}/>
