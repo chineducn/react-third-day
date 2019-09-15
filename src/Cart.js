f8910a1aa546045a8e9748a2e3514bdc8dd3273d
unknown <neduvinci@gmail.com>
2019-09-15 14:28:07 +0100

    in cart.js, showed that the ternary operator can be used instead of the if statement

--- src/Cart.js
+++ src/Cart.js
@@ -7,6 +7,16 @@ const Cart = ({ cartArray }) => {
         return <>{cartArray.map((cartItem, idx) => <h4 key={idx}>{cartItem}</h4>)}</>
     }
     return <h2>Empty Cart</h2>
+
+    // return (
+    //     <>
+    //         {
+    //             cartArray.length
+    //                 ? cartArray.map((cartItem, index) => <h5 key={index}>{cartItem}</h5>)
+    //                 : <h5>Empty Cart</h5>
+    //         }
+    //     </>
+    // )
 }
 
  
